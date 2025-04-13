a9a026172fe2400a97c4c25466d9f336c30d68d4
harichselvam <harichselvamc@gmail.com>
2025-04-13 15:40:56 +0530

    updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setup(
     name="AutoReqGen",
-    version="0.1.13",
+    version="0.1.14",
     description="Smarter pipreqs alternative with code formatting and documentation generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
